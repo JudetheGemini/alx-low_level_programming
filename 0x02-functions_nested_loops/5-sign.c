19b30f02103077dbd44251483c3662127ed22e80
Jude Ubuntu <root@JUDE.localdomain>
2022-04-12 16:42:19 +0100

    Adding 5-sign.c and updated main.h file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_sign - checks if the value is positive or negative
+ *
+ *@n: integer parameter
+ * Return: 1 if greater than zero,
+ * 0 if equal to zero
+ * -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		_putchar('-');
+	return (-1);
+}
