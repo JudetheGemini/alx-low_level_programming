a0387ba9d16c3caa7a75613b43fcab62df50f396
Jude Ubuntu <root@JUDE.localdomain>
2022-04-12 14:10:31 +0100

    Adding 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print all small letters ten times
+ *
+ */
+void print_alphabet_x10(void)
+{
+int letters;
+int count;
+count = 0;
+while (count < 10)
+{
+for (letters = 97; letters < 123; letters++)
+{
+_putchar(letters);
+}
+_putchar('\n');
+count++;
+}
+}
