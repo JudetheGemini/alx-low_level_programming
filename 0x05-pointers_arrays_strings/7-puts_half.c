29cc5ecc26a9c2fc3b94b87872f9a4e74de2678e
Jude Ubuntu <root@JUDE.localdomain>
2022-04-20 21:23:26 +0100

    correcting 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,6 +10,14 @@ void puts_half(char *str)
 	int s, k;
 
 	s = strlen(str);
+	if (s % 2 == 1)
+	{
+		for (k = (s - 1) / 2; k < s; k++)
+		{
+			_putchar(str[k]);
+		}
+	}
+	else
 	for (k = (s / 2); k < s; k++)
 	{
 		_putchar(str[k]);
