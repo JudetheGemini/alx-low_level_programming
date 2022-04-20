376a9296b24228f890047a62ba0a497d586cbc69
Jude Ubuntu <root@JUDE.localdomain>
2022-04-20 16:47:02 +0100

    adding 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strlen - returns length of a string
+ *@s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
