524bd7a39dbefcbd116f2201403348f4ccb295c4
Jude Ubuntu <root@JUDE.localdomain>
2022-04-20 22:07:42 +0100

    adding 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ **_strcpy - copy string to buffer
+ *@dest: destination
+ *@src: string source
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
