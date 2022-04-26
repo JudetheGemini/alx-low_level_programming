c833c230c39868e44462dda38dc8070f78c1f64a
Jude Ubuntu <root@JUDE.localdomain>
2022-04-27 00:08:46 +0100

    adding 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strchr - looks for a string in an array
+ *@s: array to be searched
+ *@c: character being searched for
+ * Return: pointer to character in string or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		if (*(s + j) == c)
+	 
+			return ((s + j));
+		if (*(s + j) == c)
+		{
+			return (s + j);
+		}
+	}
+	return ('\0');
+}
