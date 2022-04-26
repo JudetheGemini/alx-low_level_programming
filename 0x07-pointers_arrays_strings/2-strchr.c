a98553fac3a511be4aece818e59996efc27e2c96
Jude Ubuntu <root@JUDE.localdomain>
2022-04-27 00:15:14 +0100

    testing 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,8 +10,12 @@ char *_strchr(char *s, char c)
 	unsigned int j;
 
 	for (j = 0; s[j] != '\0'; j++)
+	{
 		if (*(s + j) == c)
+		{
 			return ((s + j));
+		}
+	}
 		if (*(s + j) == c)
 			return (s + j);
 	return ('\0');
