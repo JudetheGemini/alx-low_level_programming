0b0a51ac54c122bb19780d1fcd77375dcb4f5502
Jude Ubuntu <root@JUDE.localdomain>
2022-05-12 00:27:16 +0100

    updating dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - a dog
  * @name: name of dog
  * @age: age of dog
@@ -15,4 +20,5 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 #endif
