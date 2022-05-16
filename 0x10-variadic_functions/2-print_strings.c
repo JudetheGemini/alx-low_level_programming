3cdcab6a2ee2002323328f4916b62f0588dea108
Jude Ubuntu <root@JUDE.localdomain>
2022-05-16 22:41:32 +0100

    correcting 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,15 +11,22 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ptr;
 	unsigned int i;
+	char *str;
 
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ptr, char*));
+		str = va_arg(ptr, char *);
+		if (str)
+		printf("%s", str);
+		else
+			printf("(nil)");
+
 		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
 	}
 	printf("\n");
+	va_end(ptr);
 }
