af181cc827682744c2c885b511a9f3a8bbdd3ff4
Jude Ubuntu <root@JUDE.localdomain>
2022-05-16 21:38:38 +0100

    fix 2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,13 +12,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	sum = 0;
-	va_start(ptr, n);
-
 	if (n == 0)
-	{
 		return (0);
-	}
-	else
+
+	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ptr, int);
