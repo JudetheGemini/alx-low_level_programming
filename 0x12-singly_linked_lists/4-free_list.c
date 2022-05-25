97e887a9da5150dd66f57c6e963a50893ced4b7e
Jude Ubuntu <root@JUDE.localdomain>
2022-05-25 05:31:01 +0100

    adding 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - removes a node
+ * @head: node to perfon deletion
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	current_node = head;
+	while ((current_node != NULL))
+	{
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
