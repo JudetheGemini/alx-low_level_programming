4a602e348c80bb2c094db17deb51d59dc9c5e419
Jude Ubuntu <root@JUDE.localdomain>
2022-05-26 14:28:51 +0100

    adding all files

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of
+ * a linked list
+ * @head: head of a list.
+ *
+ * Return: head node's data.
+ */
+int pop_listint(listint_t **head)
+{
+	int headNode;
+	listint_t *h;
+	listint_t *current_node;
+
+	if (*head == NULL)
+		return (0);
+
+	current_node = *head;
+
+	headNode = current_node->n;
+
+	h = curr->next;
+
+	free(current_node);
+
+	*head = h;
+
+	return (headNode);
+}
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns sum of data in lists
+ * @head:head of a list
+ * Return: sum of all data
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
