6e07643dff51ef280d8b081162f6fe8deb6967f3
Jude Ubuntu <root@JUDE.localdomain>
2022-05-26 14:23:43 +0100

    adding 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * void free_listint - frees a listint_t list
+ * @head: node to be freed
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current_node;
+
+	while ((current_node = head) != NULL)
+	{
+		head = head->next;
+		free(current_node);
+	}
+}
