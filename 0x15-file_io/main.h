8eb8457e9d4e5b22139340076e622d0679fead1b
Jude Ubuntu <root@JUDE.localdomain>
2022-06-01 12:27:05 +0100

    first fix for header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,9 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-#include <fcnt1.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
