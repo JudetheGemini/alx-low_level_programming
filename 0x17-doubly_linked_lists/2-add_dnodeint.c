c2649e59c83f643e1d0973f0398b5e6ff1bd0bd7
Jude Ubuntu <root@JUDE.localdomain>
2022-07-09 13:18:04 +0100

    [fix] for 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
-
+	if (new == NULL)
+	{
+		dprintf(2, "Error: Can't malloc\n");
+		return (NULL);
+	}
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
